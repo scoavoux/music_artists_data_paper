6ce9b94fe16e82dc042437ca4ed368d3fa2e2d2c
Paul Coudry <p.coudry@protonmail.com>
2025-12-12 16:09:19 +0000

    clean branch of unused files

--- R/make_items.R
+++ R/make_items.R
@@ -50,103 +50,12 @@ bind_items <- function(items_old, items_new, streams){
     mutate(w_feat = 1 / n_distinct(deezer_feat_id))
   
   ## join to streams and compute weighted popularity
-  items <- items %>% 
-    inner_join(streams, by = "song_id") %>% 
-    mutate(weighted_f_n_play = w_feat * f_n_play)
+  #items <- items %>% 
+   #inner_join(streams, by = "song_id") %>% 
+    #mutate(weighted_f_n_play = w_feat * f_n_play)
   
   return(items)
 }
-
-
-  
-
-### make conflicted items
-make_conflict_items <- function(items_old, items_new, streams) {
-  
-  df <- items_old %>% 
-    inner_join(items_new, 
-               by = "song_id", 
-               suffix = c("_old", "_new")) %>%
-    group_by(song_id, deezer_id_old) %>%
-    mutate(shared_deezer_id = any(deezer_id_old == deezer_id_new)) %>% # boolean col "any shared id"
-    group_by(song_id) %>%
-    mutate(keep_song = any(!shared_deezer_id), # boolean column "keep song"
-           deezer_id_old = as.integer(deezer_id_old),
-           deezer_id_new = as.integer(deezer_id_new)) %>% 
-    ungroup() %>%
-    filter(keep_song) %>% # drop if FALSE
-    select(song_id,
-           song_title = song_title_old, # keep old only because song title never changes
-           deezer_id_old,
-           deezer_id_new) %>%
-    arrange(song_id, song_title) %>% 
-    inner_join(streams, by = "song_id")
-  
-  return(df)
-}
-
-
-#### join to names
-names_to_conflicts <- function(conflicts, names, new_names) {
-  
-  names_old <- names %>% 
-    select(deezer_id_old = artist_id, 
-           name_old_id = name)
-  
-  names_new <- names %>% 
-    select(deezer_id_new = artist_id, 
-           name_new_id = name)
-  
-  conflicts_names <- conflicts %>%
-    left_join(names_old, by = "deezer_id_old") %>% 
-    left_join(names_new, by = "deezer_id_new") %>% 
-    arrange(desc(f_n_play)) %>% 
-    select(song_title, 
-           name_old_id, 
-           name_new_id,
-           deezer_id_old, 
-           deezer_id_new,
-           n_play, 
-           f_n_play, 
-           song_id)
-  
-  ## join names scraped from deezer API
-  conflicts_names <- conflicts_names %>%
-    left_join(new_names, by = "deezer_id_new") %>%
-    mutate(name_new_id = coalesce(name_new_id, name),
-           deezer_match = ifelse(deezer_id_old == deezer_id_new, TRUE, FALSE)) %>%
-    select(-name)
-  
-  return(conflicts_names)
-}
-
-
-### join to scraped names
-
-
-### filter conflicts to match
-filter_conflicts_to_match <- function(conflicts_names) {
-    removed <- conflicts_names %>% 
-    group_by(song_id, deezer_id_old) %>%
-    filter(any(deezer_id_old == deezer_id_new)) %>% 
-    ungroup() %>% 
-    filter(deezer_id_old == deezer_id_new) %>% 
-    select(song_id, deezer_id_new)
-  
-  to_match <- conflicts_names %>% 
-    group_by(song_id, deezer_id_old) %>% 
-    filter(!any(deezer_id_old  == deezer_id_new)) %>% 
-    ungroup() %>% 
-    anti_join(removed) %>% 
-    distinct(deezer_id_old, deezer_id_new, .keep_all = TRUE) %>% ## took unique pairs !! important
-    select(song_id, song_title, deezer_id_old, deezer_id_new, f_n_play, name_old_id, name_new_id)
-  
-  
-  write_csv2(to_match, "data/interim/conflicts_to_match.csv")
-  
-}
-
-
 
 
 
--- notebooks/simple_id_merge.R
+++ notebooks/simple_id_merge.R
@@ -45,22 +45,113 @@ items <- items %>%
          deezer_id,
          deezer_feat_id)
 
-## weight by n featured artists
+## weight by n featured artists and join to streams
 items <- items %>%
+  inner_join(streams, by = "song_id") %>% 
   group_by(song_id) %>%
-  mutate(w_feat = 1 / n_distinct(deezer_feat_id))
+  mutate(w_feat = 1 / n_distinct(deezer_feat_id),
+         weighted_f_n_play = w_feat * f_n_play)
+
+
+# share of plays covered by items
+sum(items$f_n_play * items$w_feat)
+
+
+items %>%
+  group_by(song_id) %>%
+  summarise(f_n_play = sum(n_play) / sum(total_plays))
+
+
+unweight <- items %>% 
+  arrange(desc(f_n_play)) %>% 
+  select(f_n_play)
+
+unweight
+
+
+weight <- items %>% 
+  arrange(desc(weighted_f_n_play)) %>% 
+  select(weighted_f_n_play)
+
+weight
+
+cor.test(unweight$f_n_play, 
+         weight$weighted_f_n_play, 
+         method = 'spearman')
+
+
+items %>% 
+  filter(w_feat < 1) %>% 
+  distinct(song_id, .keep_all = T)
+
+streams %>% 
+  distinct(song_id, .keep_all = T)
 
 
 
-## join to streams
-
-
-
-i <- items %>% 
+old <- items_old %>% 
   inner_join(streams, by = "song_id")
 
-i <- i %>% 
+sum(old$f_n_play)
+
+
+new <- items_new %>% 
+  inner_join(streams, by = "song_id")
+
+both <- items %>% 
+  inner_join(streams, by = "song_id") %>% 
+  mutate(w_feat = 1 / n_distinct(deezer_feat_id))
+
+both
+
+
+
+
+
+df <- load_s3("records_w3/items/songs.snappy.parquet",
+              col_select = c("song_id",
+                             "artist_id",
+                             "artists_ids",
+                             "song_title")) %>% 
+
+  select(song_id,
+         song_title,
+         deezer_feat_id = "artists_ids",
+         deezer_id = "artist_id")
+
+
+df2 <- df %>% 
+  mutate(deezer_feat_id = map_chr(deezer_feat_id, 
+                                  ~ paste(as.integer(.x), 
+                                          collapse = ","))) %>% 
+  filter(!is.na(deezer_id)) %>% 
+  separate_rows(deezer_feat_id, sep = ",") %>% 
+  select(song_id,
+         song_title,
+         deezer_id,
+         deezer_feat_id)
+
+df3 <- df2 %>%
+  group_by(song_id) %>% 
+  mutate(w_feat = 1 / n_distinct(deezer_feat_id)) %>% 
+  inner_join(streams, by = "song_id") %>% 
   mutate(weighted_f_n_play = w_feat * f_n_play)
+
+
+
+options(scipen = 99)
+df
+sum(df$weighted_f_n_play)
+
+
+
+
+
+
+
+
+
+
 
 
 
