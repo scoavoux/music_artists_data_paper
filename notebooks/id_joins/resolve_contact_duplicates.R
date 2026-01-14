0c9cf53c3ed41069201d91109c68246396fe9d24
scoavoux <samuel.coavoux@gmail.com>
2026-01-14 15:34:02 +0000

    implement resolving contacts duplicates
    i run some checks on the metrics to use for identifying the "right" duplicates in contacts. from what i see, the big majority of cases can be solved right away by setting the share of collection_count > 0.9 (or some other threshold). adding other conditions (like product_count, or gen_like_count) only adds a few dozens cases and it gets a bit messy because the columns contain many zeros. so for now i only keep collection_count, but we can ofc easily change this.
    
    finally, i add ~1700 cases (with my 2 helper functions, like the rest) we could retrieve by disentangling some contacts duplicates to all.
    
    next step is to look at the cases still missing: who are they, why are they still missing, and how could we retrieve them?
    
    also, need to check how to implement all of this in targets without getting lost, since it is basically the same operation many times.

---
+++ notebooks/id_joins/resolve_contact_duplicates.R
@@ -0,0 +1,64 @@
+# some contact_names cannot be safely linked to a deezer name because there are duplicates
+# among those duplicates, check nr of reviews etc to see if some of them are way more popular
+# same logic as identify_deezer_duplicates.R!
+
+contacts <- load_s3("senscritique/contacts.csv")
+
+# for each name in contacts, compute popularity of occurrences
+## col_share: share of collection_counts
+## prod_share: share of songs (?)
+## gen_like_share: share of likes
+contacts <- as_tibble(contacts) %>% 
+  select(-c(contact_name_url, subtype_id, spotify_id,
+            mbz_id, freebase_id)) %>% 
+  # remove cases with no 
+  filter(product_count > 0 & gen_like_count > 0 & collection_count > 0) %>% 
+  group_by(contact_name) %>% 
+  mutate(col_share = collection_count / sum(collection_count),
+         prod_share = product_count / sum(product_count),
+         gen_like_share = gen_like_count / sum(gen_like_count))
+
+
+# ------- join to missing contact_data in all
+
+# keep this condition for now: adding the other variables adds like 30 cases
+# but unsure about the cases (e.g., there are weird ones with very few albums)
+co_unique <- contacts %>% 
+  filter(col_share > 0.9) %>% 
+  select(contact_name, contact_id)
+
+# important: subset all to unique names missing in contacts
+all_unique_co <- all %>% 
+  add_count(name) %>% 
+  filter(n == 1) %>% 
+  filter(is.na(contact_id))
+
+added_contacts <- unique_name_match(
+  miss = all_unique_co,
+  ref = co_unique,
+  miss_name = "name",
+  ref_name = "contact_name",
+  id_col = "contact_id"
+)
+
+all <- left_join_coalesce(
+  all,
+  added_contacts,
+  by = "deezer_id",
+  col = "contact_id"
+)
+
+cleanpop(all)
+
+
+## anti_join etc to subset cases still missing
+all_unique_co %>% 
+  anti_join(added_contacts, by = "deezer_id")
+
+
+
+
+
+
+
+
