9e0f26a2de0e572c75cd9bca325b3a8e2c426f58
scoavoux <samuel.coavoux@gmail.com>
2026-01-15 10:11:49 +0000

    add layout opts to init
    add my custom theme (merbivore soft) to the initialization script. also, simplify package installation.
    
    i could check again how to customize the pane layout, since i did not manage to do it here i think (see the corresponding chunk though)

--- init.R
+++ init.R
@@ -1,8 +1,46 @@
-install.packages("targets")
-install.packages("tarchetypes")
-install.packages("visNetwork")
-install.packages("aws.s3")
-install.packages("tidytable")
-install.packages("logging")
-install.packages("dplyr")
-# install.packages("tidyverse")
+
+
+needed.packages <- c('targets',
+                     'tarchetypes',
+                     'visNetwork',
+                     'aws.s3',
+                     'tidytable',
+                     'logging',
+                     'dplyr',
+                     'WikidataQueryServiceR',
+                     'rstudioapi')
+
+
+for(pack in needed.packages){
+  if(pack %in% rownames(installed.packages()) == FALSE)
+  {install.packages(pack)}
+}
+
+
+# set theme and pane layout 
+
+rstudioapi::applyTheme("Merbivore Soft")
+
+rstudioapi::writeRStudioPreference("pane_config", list(
+     console = "left",
+     source = "right",
+     tabSet1 = "right",
+     tabSet2 = "right",
+     hiddenTabSet = "none"
+     ))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
