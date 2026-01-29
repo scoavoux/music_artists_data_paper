392378dd81c44066477b81062e08cb1bfae96914
paulcdry <p.coudry@protonmail.com>
2026-01-29 11:12:19 +0000

    decide on disambiguation strategy
    sam and i decide to disambiguate everything we can, i.e. choose the more legitimate artist, instead of keeping duplicate ids etc. this will simplify things a lot downstream and it is probably much safer than making assumptions on the duplicate's nature
    
    my strategy: use pop_share for contacts and mbz duplicates (i.e. multiple deezer_ids for one co/mbz id), and collection_count for deezer duplicates. see how far we get with that and how to properly join this into the data again.
    
    we also agreed on maybe solving some cases manually, like the most important ones.

--- notebooks/solve_deezer_dups.R
+++ notebooks/solve_deezer_dups.R
@@ -1,78 +1,45 @@
+# CONTACTS! (i.e. different deezer_ids)
+
 library(sjmisc)
 
 tar_load(all_enriched)
 
-all_count <- all_enriched %>% 
-  filter(!is.na(musicbrainz_id) | !is.na(contact_id)) %>% 
-  add_count(deezer_id, name = "n_deezer") %>% 
-  add_count(musicbrainz_id, name = "n_mbz") %>% 
-  add_count(contact_id, name = "n_co")
+## work separately for co and mbz: subset !is.na(co)
+## then !is.na(mbz)
+## then solve deezer through collection count!
 
-all_dups <- all_count %>% 
-  filter(n_deezer > 1 |
-           n_mbz > 1 |
-           n_co > 1)
+col_count <- contacts %>% 
+  select(contact_id, collection_count)
 
-
-###### for co + mbz: TEST pop_share for disambiguation!
-co_dups <- all_dups %>% 
+co_dup <- all_enriched %>% 
+  filter(!is.na(contact_id)) %>% 
+  left_join(col_count, by = "contact_id") %>% 
+  #add_count(deezer_id, name = "n_deezer") %>% 
+  #add_count(musicbrainz_id, name = "n_mbz") %>% 
+  add_count(contact_id, name = "n_co") %>% 
   filter(n_co > 1) %>%
+  # create pop share disambiguation col
+  group_by(contact_id) %>%
+  mutate(max_pop = max(pop, na.rm = TRUE),
+         pop_share = pop / sum(pop)) %>%
+  arrange(desc(max_pop), desc(pop))
+
+
+
+###### use pop_share to disambiguate!
+co_dups_to_keep <- co_dup %>% 
   group_by(contact_id) %>%
   mutate(max_pop = max(pop, na.rm = TRUE),
          pop_share = pop / sum(pop)) %>%
   arrange(desc(max_pop), desc(pop)) %>%
   ungroup() %>% 
-  #filter(!is.na(contact_id)) %>% 
-  select(name, contact_name, deezer_id, contact_id, pop, pop_share)
+  filter(!is.na(contact_id)) %>% 
+  select(name, contact_name, deezer_id, 
+         contact_id, pop, pop_share, n_co)
 
-co_dups %>%
+t <- co_dups %>% 
   filter(pop_share > 0.9)
 
-mbz_dups <- all_dups %>% 
-  filter(n_mbz > 1) %>%
-  group_by(musicbrainz_id) %>%
-  mutate(max_pop = max(pop, na.rm = TRUE),
-         pop_share = pop / sum(pop)) %>%
-  arrange(desc(max_pop), desc(pop)) %>%
-  ungroup() %>% 
-  #filter(!is.na(contact_id)) %>% 
-  select(name, mbz_name, deezer_id, musicbrainz_id, pop, pop_share)
-
-mbz_dups %>%
-  filter(pop_share < 0.9 & pop_share > 0.01 & pop_share != 0.5)
 
 
-
-deezerdups %>% 
-  distinct(contact_id, musicbrainz_id, deezer_id)
-
-deezerdups <- deezerdups %>%
-  left_join(contacts %>% 
-              select(contact_id, collection_count),
-            by = "contact_id")
-
-t <- deezerdups %>% 
-  filter(collection_count > 0) %>% # remove irrelevant artists 
-  group_by(deezer_id) %>% 
-  mutate(collection_count = as.integer(collection_count)) %>% 
-  mutate(col_share = collection_count / sum(collection_count)) #%>% 
-  #filter(col_share > 0.9)
-
-
-
-
-
-### TO WIDE (for export??)
-for_wide <- all_dups %>% 
-  filter(n_co > 5) # drop extreme cases
-
-df_wide <- all_dups %>%
-  group_by(deezer_id) %>%
-  mutate(dup_n = row_number()) %>%
-  pivot_wider(
-    id_cols = c(deezer_id, name),
-    names_from = dup_n,
-    values_from = c(contact_id, musicbrainz_id),
-    names_glue = "{.value}_{dup_n}"
-  )
 
