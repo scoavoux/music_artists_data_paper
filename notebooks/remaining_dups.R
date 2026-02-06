fc7ac522d38857521a31a92881c5b92110828041
paulcdry <p.coudry@protonmail.com>
2026-02-06 11:02:25 +0000

    solve dups definitevely
    finally figured out a way to solve duplicates without touching NAs. some duplicates remain even after this because they could not be separated (e.g. both collection_count == 0), but they do not account for a large stream_share. in any case it is easy to retrieve them --- maybe save those (and the ones we dropped) as byproducts of the target for later checks.
    
    see if there is a way to shorten this little pipeline by functionalizing it. not sure if this makes so much sense though, the way i do it here is easier to follow than if i wrap it into a function.

--- notebooks/remaining_dups.R
+++ notebooks/remaining_dups.R
@@ -2,9 +2,8 @@ library(dplyr)
 library(stringr)
 
 tar_load(all_enriched)
-
+cleanpop(all_enriched)
 # -----------------------------------------------
-
 
 ## DELETE COLLECTION_COUNT == 0 before counting dups
 
@@ -13,171 +12,91 @@ tar_load(all_enriched)
 ## run this after all enrichment steps so i have complete cases ? --> !
 
 
-# --------------------------------------------------------------------------------
-
-tar_load(all_enriched)
-
-all <- all_enriched %>% 
-  add_count(deezer_id, name = "n_deezer") %>%
-  add_count(contact_id, name = "n_co") %>%
-  add_count(musicbrainz_id, name = "n_mbz") #%>% 
-  #filter(n_deezer > 1 | n_co > 1 | n_mbz > 1)
-  
-
-# compute score share columns
-# could add mbz too, but it concerns almost no cases
-all <- all %>% 
-  #filter(!is.na(contact_id)) %>%
-  group_by(deezer_id) %>% 
-  mutate(dz_col_share = collection_count / sum(collection_count)) %>% 
-  ungroup() %>% 
-  group_by(contact_id) %>% 
-  mutate(co_pop_share = pop / sum(pop)) %>% 
-  ungroup() 
-
-to_keep_dz <- all %>% 
-  filter(n_deezer > 1) %>% # duplicates
-  group_by(deezer_id) %>%
-  mutate(dz_col_share = collection_count / sum(collection_count)) %>% # create score_share 
-  filter(dz_col_share == max(dz_col_share)) ## formerly score_share > threshold
-
-to_keep_co <- all %>% 
-  filter(n_co > 1) %>% # duplicates
-  group_by(contact_id) %>%
-  mutate(co_pop_share = collection_count / sum(collection_count)) %>%
-  filter(co_pop_share == max(co_pop_share))
-
-to_keep_mbz <- all %>% 
-  filter(n_mbz > 1) %>% # duplicates
-  group_by(musicbrainz_id) %>%
-  mutate(mbz_pop_share = collection_count / sum(collection_count)) %>%
-  filter(mbz_pop_share == max(mbz_pop_share))
-
-
-all_enriched %>% 
-  filter(deezer_id == 10336)
-
-
-to_keep <- to_keep_co %>% 
-  select(-c(n_deezer, n_co, n_mbz, dz_col_share, co_pop_share))
-
-upd <- all_enriched %>% 
-  rows_update(to_keep, by = "contact_id")
-  
-
-
-test <- to_keep_dz %>% 
-  rows_update(to_keep_co, by = "deezer_id") %>% 
-  rows_update(to_keep_mbz, by = "deezer_id") %>% 
-  rows
-  
-
-
-# bind "winners"
-all_enriched <- all_enriched %>% 
-  anti_join(to_keep_dz, by = "deezer_id") %>% 
-  bind_rows(to_keep_dz) %>% 
-  anti_join(to_keep_co, by = "deezer_id") %>% 
-  bind_rows(to_keep_co) %>% 
-  anti_join(to_keep_mbz, by = "deezer_id") %>% 
-  bind_rows(to_keep_mbz)
-
-  
-
-t <- all_enriched %>% 
-  filter(!is.na(contact_id) & !is.na(musicbrainz_id)) %>% 
-  add_count(deezer_id, name = "n_deezer") %>%
-  add_count(contact_id, name = "n_co") %>%
-  add_count(musicbrainz_id, name = "n_mbz") %>% 
-  filter(n_deezer > 1 | n_co > 1 | n_mbz > 1)
-
-
-test <- t %>% 
-  filter(!is.na(contact_id) & !is.na(musicbrainz_id))
-
-
 # ------------------------------------------------------------------
-tar_load(all_enriched)
+
+# compute shares to determine winners
 
 all <- all_enriched %>% 
+  
   group_by(deezer_id) %>% 
-  mutate(
-    dz_col_share = if_else(
-      !is.na(deezer_id),
-      collection_count / sum(collection_count, na.rm = TRUE),
-      NA_real_
-    )
-  ) %>% 
+  mutate(dz_col_share = collection_count / sum(collection_count, na.rm = TRUE))  %>% 
   ungroup() %>% 
-  group_by(contact_id) %>% 
-  mutate(
-    co_pop_share = if_else(
-      !is.na(contact_id),
-      pop / sum(pop, na.rm = TRUE),
-      NA_real_
-    )
-  ) %>% 
-  ungroup() %>% 
-  group_by(musicbrainz_id) %>% 
-  mutate(
-    mbz_pop_share = if_else(
-      !is.na(musicbrainz_id),
-      pop / sum(pop, na.rm = TRUE),
-      NA_real_
-    )
-  ) %>% 
-  ungroup()
+  
+  group_by(contact_id) %>%
+  mutate(co_pop_share = pop / sum(pop, na.rm = TRUE)) %>%
+  ungroup() %>%
 
+  group_by(musicbrainz_id) %>%
+  mutate(mbz_pop_share = pop / sum(pop, na.rm = TRUE)) %>%
+  ungroup()
 
 # ------------------------------------------------------
 
-allcount <- all %>% 
+# only keep conflicts to speed up conflict identification
+dz_conflicts <- all %>% 
   add_count(deezer_id, name = "n_dz") %>% 
+  mutate(keep_dz = is.na(deezer_id) | n_dz == 1) %>% 
+  filter(keep_dz == FALSE)
+
+co_conflicts <- all %>% 
   add_count(contact_id, name = "n_co") %>% 
+  mutate(keep_co = is.na(contact_id) | n_co == 1) %>% 
+  filter(keep_co == FALSE)
+
+mbz_conflicts <- all %>% 
   add_count(musicbrainz_id, name = "n_mbz") %>% 
-  mutate(
-    keep_dz  = is.na(deezer_id) | n_dz == 1,
-    keep_co  = is.na(contact_id) | n_co == 1,
-    keep_mbz = is.na(musicbrainz_id) | n_mbz == 1
-  ) %>% 
-  mutate(.row_id = row_number())
+  mutate(keep_mbz = is.na(musicbrainz_id) | n_mbz == 1) %>% 
+  filter(keep_mbz == FALSE)
 
+# ---------------------------------------------------------
 
-# FILTER CONFLICTS
-dz_conflicts <- allcount %>% 
-  filter(!is.na(deezer_id), n_dz > 1) %>% 
+dz_losers <- dz_conflicts %>% 
   group_by(deezer_id) %>% 
   mutate(keep_dz = dz_col_share == max(dz_col_share, na.rm = TRUE)) %>% 
-  ungroup() #%>% 
-  #select(.row_id, keep_dz)
+  filter(keep_dz == FALSE)
 
-co_conflicts <- allcount %>% 
-  filter(!is.na(contact_id), n_co > 1) %>% 
+co_losers <- co_conflicts %>% 
   group_by(contact_id) %>% 
   mutate(keep_co = co_pop_share == max(co_pop_share, na.rm = TRUE)) %>% 
-  ungroup() %>% 
-  select(.row_id, keep_co)
+  filter(keep_co == FALSE)
 
-mbz_conflicts <- allcount %>% 
-  filter(!is.na(musicbrainz_id), n_mbz > 1) %>% 
+mbz_losers <- mbz_conflicts %>% 
   group_by(musicbrainz_id) %>% 
   mutate(keep_mbz = mbz_pop_share == max(mbz_pop_share, na.rm = TRUE)) %>% 
-  ungroup() %>% 
-  select(.row_id, keep_mbz)
-
-allcount$keep_dz[dz_conflicts$row_id] <- dz_conflicts$keep_dz
-allcount$keep_co[co_conflicts$row_id] <- co_conflicts$keep_co
-allcount$keep_mbz[mbz_conflicts$row_id] <- mbz_conflicts$keep_mbz
-
+  filter(keep_mbz == FALSE)
 
 # -------------------------------------------------------
 
-all_dedup <- allcount %>% 
-  filter(keep_dz & keep_co & keep_mbz)
+# remove from all
 
-table(allcount$co_pop_share)
+all_dedup <- all %>% 
+  anti_join(dz_losers, by = c("deezer_id", "contact_id", "musicbrainz_id")) %>% 
+  anti_join(co_losers, by = c("deezer_id", "contact_id", "musicbrainz_id")) %>% 
+  anti_join(mbz_losers, by = c("deezer_id", "contact_id", "musicbrainz_id")) %>% 
+  
+  # count remaining duplicates
+  add_count(deezer_id, name = "n_deezer") %>% 
+  add_count(contact_id, name = "n_co") %>% 
+  add_count(musicbrainz_id, name = "n_mbz") %>% 
+  
+  # set NA
+  mutate(
+    n_deezer = ifelse(n_deezer > 1000, NA, n_deezer),
+    n_co = ifelse(n_co > 1000, NA, n_co),
+    n_mbz = ifelse(n_mbz > 1000, NA, n_mbz)
+  )
 
-cleanpop(all_dedup)
+
+all_dedup <- all_dedup %>% 
+  filter(n_deezer == 1 | is.na(n_deezer)) %>% 
+  filter(n_co == 1 | is.na(n_co)) %>% 
+  filter(n_mbz == 1 | is.na(n_mbz))
+  
+  
+## check filtered out cases
+t <- all %>% 
+  anti_join(all_dedup, by = c("deezer_id", "contact_id", "musicbrainz_id"))
+
 
 
 
