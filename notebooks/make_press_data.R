851ab37d7b5ef72c53bd4e808f2204a6811cd80a
paulcdry <p.coudry@protonmail.com>
2026-02-17 11:14:59 +0000

    add make alias function

--- notebooks/make_press_data.R
+++ notebooks/make_press_data.R
@@ -281,67 +281,6 @@ make_aliases <- function(senscritique_mb_deezer_id, artists_pop){
   artist_names_and_aliases <- consolidated_id_name_alias %>% 
     mutate(regex = regexify(name))
   return(artist_names_and_aliases)
-  
-  # require(WikidataQueryServiceR)
-  # 
-  # # Get aliases from Wikidata
-  # wikidata_spotifyid_aliases_en <- query_wikidata('SELECT DISTINCT ?spotify_id ?item ?itemLabel ?itemAltLabel
-  # WHERE {
-  #   SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-  #   ?item p:P1902 ?statement0.
-  #   ?statement0 (ps:P1902) _:anyValueP1902.
-  #   ?item wdt:P1902 ?spotify_id.
-  # }')
-  # wikidata_spotifyid_aliases_fr <- query_wikidata('SELECT DISTINCT ?spotify_id ?item ?itemLabel ?itemAltLabel
-  # WHERE {
-  #   SERVICE wikibase:label { bd:serviceParam wikibase:language "fr". }
-  #   ?item p:P1902 ?statement0.
-  #   ?statement0 (ps:P1902) _:anyValueP1902.
-  #   ?item wdt:P1902 ?spotify_id.
-  # }')
-  # wikidata_spotifyid_aliases <- bind_rows(wikidata_spotifyid_aliases_en, 
-  #                                         wikidata_spotifyid_aliases_fr) %>% 
-  #   distinct()
-  # 
-  # 
-  # wikidata_deezerid_aliases_en <- query_wikidata('SELECT DISTINCT ?deezer_id ?item ?itemLabel ?itemAltLabel
-  # WHERE {
-  #   SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-  #   ?item p:P2722 ?statement1.
-  #   ?statement1 (ps:P2722) _:anyValueP2722.
-  #   ?item wdt:P2722 ?deezer_id.
-  # }')
-  # wikidata_deezerid_aliases_fr <- query_wikidata('SELECT DISTINCT ?deezer_id ?item ?itemLabel ?itemAltLabel
-  # WHERE {
-  #   SERVICE wikibase:label { bd:serviceParam wikibase:language "fr". }
-  #   ?item p:P2722 ?statement1.
-  #   ?statement1 (ps:P2722) _:anyValueP2722.
-  #   ?item wdt:P2722 ?deezer_id.
-  # }')
-  # wikidata_deezerid_aliases <- bind_rows(wikidata_deezerid_aliases_en, 
-  #                                        wikidata_deezerid_aliases_fr) %>% 
-  #   distinct()
-  # 
-  # wikidata_mbid_aliases_fr <- query_wikidata('SELECT DISTINCT ?mbid ?item ?itemLabel ?itemAltLabel
-  # WHERE {
-  #   SERVICE wikibase:label { bd:serviceParam wikibase:language "fr". }
-  #   ?item p:P434 ?statement0.
-  #   ?statement0 (ps:P434) _:anyValueP434.
-  #   ?item wdt:P434 ?mbid.
-  # }')
-  # 
-  # wikidata_mbid_aliases_en <- query_wikidata('SELECT DISTINCT ?mbid ?item ?itemLabel ?itemAltLabel
-  # WHERE {
-  #   SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-  #   ?item p:P434 ?statement0.
-  #   ?statement0 (ps:P434) _:anyValueP434.
-  #   ?item wdt:P434 ?mbid.
-  # }')
-  # 
-  # wikidata_mbid_aliases <- bind_rows(wikidata_mbid_aliases_en, 
-  #                                    wikidata_mbid_aliases_fr) %>% 
-  #   distinct()
-  
 }
 
 make_artists_names <- function(artist_names_and_aliases){
