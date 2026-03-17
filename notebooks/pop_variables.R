44d2b17b5719b584bec41629d6beab0a75549c17
paulcdry <p.coudry@protonmail.com>
2026-03-17 09:36:59 +0000

    this is it: pop workflow idea

--- notebooks/pop_variables.R
+++ notebooks/pop_variables.R
@@ -13,6 +13,7 @@ users <- load_s3("records_w3/RECORDS_hashed_user_group.parquet")
 users <- users %>% 
   mutate(is_respondent = ifelse(is_respondent == TRUE, 1, 0),
          is_control = ifelse(is_in_control_group == TRUE, 1, 0)) %>% 
+  filter(is_respondent != 0 | is_control != 0) %>% # remove users who are neither
   select(hashed_id, is_respondent, is_control)
 
 
@@ -21,76 +22,75 @@ users <- users %>%
 streams <- arrow::open_dataset(
   source = arrow::s3_bucket("scoavoux",
                               endpoint_override = "minio.lab.sspcloud.fr")
-  $path("records_w3/new_streams/streams_all")
+  $path("records_w3/streams/streams_short")
 )
 
-streams <- streams %>%
-  filter(
-    year > 2009 & year < 2025,
-    is_listened == 1,
-    song_id > 0 
-  ) %>%
-  mutate(lt = ifelse(listening_time < 0, 0, listening_time)) %>% 
-  group_by(hashed_id, song_id) %>%
-  summarise(
-    n_play = n(),
-    l_play = sum(lt),
-    .groups = "drop"
-  ) %>% 
-  select(hashed_id, song_id, n_play, l_play)
-  
-
-
-# ---------------------------------------------------------------
-# ATTACH USER GROUPS
-# AND MAKE 2 SUBSETS (RESPONDENTS AND CONTROL)
-streams <- streams %>%
-  left_join(users, by = "hashed_id")
-
-streams_control <- streams %>% 
-  filter(is_control == 1)
-
-streams_respondent <- streams %>% 
-  filter(is_respondent == 1)
-
-
-# ---------------------------------------------------------------
-# SONG → ARTIST JOIN
-
-streams_control <- streams_control %>%
-  inner_join(song_artist_weights, by = "song_id") %>%
-  mutate(
-    n_play = n_play * w_feat,
-    l_play = l_play * w_feat
-  ) %>% 
-  group_by(dz_artist_feat_id) %>%
-  summarise(
-    n_play_control = sum(n_play),
-    l_play_control = sum(l_play),
-    .groups = "drop"
-  ) %>%
+streams <- streams %>% 
+  select(hashed_id, 
+         is_listened, 
+         ts_listen, 
+         listening_time, 
+         media_type, 
+         song_id = "media_id") %>% 
+  mutate(year = year(as_datetime(ts_listen)),
+         lt = ifelse(listening_time < 0, 0, listening_time)) %>% 
+  filter(media_type == "song", 
+         is_listened == 1, 
+         year == 2022,
+         song_id > 0) %>% 
+  inner_join(users, by = "hashed_id") %>% 
+  filter(is_respondent == 1) %>% 
+  group_by(song_id) %>% 
+  summarize(n_play = n(),
+            l_play = sum(listening_time)) %>% 
+  select(song_id, n_play, l_play) %>% 
   collect()
 
 
 # ---------------------------------------------------------------
 # ARTIST POPULARITY
-
-artist_popularity <- user_artist_groups %>%
+artist_popularity <- streams %>%
+  inner_join(song_artist_weights, by = "song_id") %>% 
+  mutate(n_play = n_play * w_feat,
+         l_play = l_play * w_feat) %>% 
   group_by(dz_artist_feat_id) %>%
   summarize(
-    n_play_control = sum(n_play * is_control, na.rm = TRUE),
-    l_play_control = sum(l_play * is_control, na.rm = TRUE),
-    n_play_respondent = sum(n_play * is_respondent, na.rm = TRUE),
-    l_play_respondent = sum(l_play * is_respondent, na.rm = TRUE),
+    n_play_respondent = sum(n_play),
+    l_play_respondent = sum(l_play),
     .groups = "drop"
   ) %>%
   mutate(
-    f_n_play_control = n_play_control / sum(n_play_control),
     f_n_play_respondent = n_play_respondent / sum(n_play_respondent),
-    f_l_play_control = l_play_control / sum(l_play_control),
     f_l_play_respondent = l_play_respondent / sum(l_play_respondent)
-  ) %>%
-  collect()
+  )
+
+tar_load(all_final)
+
+t <- all_final %>% 
+  left_join(artist_popularity, by = c(dz_artist_id = "dz_artist_feat_id"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- notebooks/pop_variables_old.R
+++ notebooks/pop_variables_old.R
@@ -17,12 +17,8 @@ users <- users %>%
 
 
 ## HEY HEY HEY
-## SEPARATE STREAMS COMPLETELY BY CONTROL/RESPONDENT
-## THEN JUST NEED TO MAKE THE W_FEAT THING AND
-## WE CAN AGGREGATE BY ARTIST
-
-
-
+## FORGET ABOUT W_FEAT FOR NOW AND
+## MAKE USER-ARTIST SCORES
 
 
 # --------------------------------------------------
@@ -32,11 +28,12 @@ data_cloud <- arrow::open_dataset(
   source = arrow::s3_bucket(
     "scoavoux",
     endpoint_override = "minio.lab.sspcloud.fr"
-  )$path("records_w3/streams/streams_short"),
+  )$path("records_w3/streams/streams_long"),
   partitioning = arrow::schema(REGION = arrow::utf8())
 )
 
-months <- sprintf("2022-%02d", 1:12)
+
+months <- sprintf("2018-%02d", 1:12)
 
 results <- list()
 
@@ -49,15 +46,15 @@ for (m in months) {
       hashed_id,
       ts_listen,
       listening_time,
-      media_type,
-      song_id = media_id
+      #media_type,
+      song_id
     ) %>%
     mutate(
       month = format(as_datetime(ts_listen), "%Y-%m"),
       lt = pmax(listening_time, 0)
     ) %>%
     filter(
-      media_type == "song",
+      #media_type == "song",
       month == m
     ) %>%
     inner_join(song_artist_weights, by = "song_id") %>%
@@ -76,7 +73,22 @@ for (m in months) {
   results[[m]] <- query
 }
 
-streams_small <- bind_rows(results)
+streams_2018 <- bind_rows(results)
+
+
+
+
+
+
+streams_2023 <- bind_rows(results)
+
+
+
+
+
+
+
+
 
 streams_artist <- streams_small %>%
   group_by(hashed_id, dz_artist_feat_id) %>%
@@ -89,84 +101,6 @@ streams_artist <- streams_small %>%
 
 
 
-# -----------------------------------------------------
-
-years  <- 1970:2098
-months <- 1:12
-
-results <- list()
-i <- 1
-
-data_cloud <- arrow::open_dataset(
-  source = arrow::s3_bucket(
-    "scoavoux",
-    endpoint_override = "minio.lab.sspcloud.fr"
-  )$path("records_w3/streams/streams_long")
-)
-
-for (y in years) {
-  #for (m in months) {
-    
-    cat("Processing", y, #m, 
-        "\n")
-    
-    query <- data_cloud %>%
-      select(
-        hashed_id,
-        ts_listen,
-        listening_time,
-        #media_type,
-        song_id
-      ) %>%
-      mutate(
-        year = format(as_datetime(ts_listen), "%Y"),
-        # month = format(as_datetime(ts_listen), "%Y-%m"),
-        lt = pmax(listening_time, 0)
-      ) %>%
-      filter(
-        year == y,
-        #month == m,
-      ) %>%
-      inner_join(song_artist_weights, by = "song_id") %>%
-      
-      # collapse streams first
-      group_by(hashed_id, song_id, dz_artist_feat_id, w_feat) %>%
-      summarise(
-        n_play = n(),
-        l_play = sum(lt),
-        .groups = "drop"
-      ) %>%
-      
-      # apply weights
-      mutate(
-        n_play = n_play * w_feat,
-        l_play = l_play * w_feat
-      ) %>%
-      
-      # collapse to artist level
-      group_by(hashed_id, dz_artist_feat_id) %>%
-      summarise(
-        n_play = sum(n_play),
-        l_play = sum(l_play),
-        .groups = "drop"
-      ) %>%
-      
-      collect()
-    
-    results[[i]] <- query
-    i <- i + 1
-  #}
-}
-
-streams_all <- bind_rows(results)
-
-streams_all <- streams_all %>%
-  group_by(hashed_id, dz_artist_feat_id) %>%
-  summarise(
-    n_play = sum(n_play),
-    l_play = sum(l_play),
-    .groups = "drop"
-  )
 
 
 
